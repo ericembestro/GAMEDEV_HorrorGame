c2d2f781a5ab5007aa451da01e803fe9661898dd
Jonn Ralge Yuvallos <jonn_yuvallos@dlsu.edu.ph>
2018-11-28 22:08:04 +0800

    Bat nawala lmaoooo

---
+++ Assets/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("BANG!");
+            GetComponent<Animation>().Play();
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
