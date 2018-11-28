bc9d8ba9dc9754bbe0e36da8432a2d2222c58cde
ericembestro <japan569@gmail.com>
2018-11-28 18:29:53 +0800

    TitleScreen

---
+++ Assets/Scripts/Views/TitleScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : View {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnStartButtonClick() {
+        Debug.Log("*Start*");
+        //Load the next scene when start button is clicked
+        //LoadManager.Instance.LoadScene("");
+    }
+
+    public void OnExitButtonClick() {
+        Debug.Log("*Exit*");
+        //Application.Quit();
+    }
+}
