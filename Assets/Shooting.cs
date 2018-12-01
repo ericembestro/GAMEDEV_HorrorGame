fa3f9de3cc8e503a28589037b12e0a392c3ae48b
Jonn Ralge Yuvallos <jonn_yuvallos@dlsu.edu.ph>
2018-12-01 14:47:10 +0800

    Added zombie model, Improved the gun model and can look around and shoot now
    Still no raycasting and bullet damage on the zombies

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -3,17 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour {
-	void Start () {
-		
-	}
+    [SerializeField] private Animator GunAnimator;
+    private const string SHOOT_KEY = "Shooting";
+    private AudioSource bangSound;
+    private Light muzzleFlash;
+    void Start () {
+        bangSound = GetComponent<AudioSource>();
+        muzzleFlash = GetComponent<Light>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("BANG!");
-            GetComponent<Animation>().Play();
-            GetComponent<AudioSource>().Play();
+            this.GunAnimator.SetBool(SHOOT_KEY, true);
+            bangSound.loop = true;
+            bangSound.Play();
+            muzzleFlash.enabled = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("STOP BANG!");
+            bangSound.loop = false;
+            bangSound.Pause();
+            this.GunAnimator.SetBool(SHOOT_KEY, false);
+            muzzleFlash.enabled = false;
         }
     }
 }
