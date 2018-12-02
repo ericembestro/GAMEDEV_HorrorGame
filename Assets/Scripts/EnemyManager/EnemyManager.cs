a4e3d6043aa860fa410d4ddb3c0d1472dc9a92a1
isiahmacs <isiah_macalalad@dlsu.edu.ph>
2018-12-02 16:58:32 +0800

    made the spawn and movement better.. animation still stationary

--- Assets/Scripts/EnemyManager/EnemyManager.cs
+++ Assets/Scripts/EnemyManager/EnemyManager.cs
@@ -1,41 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyManager : MonoBehaviour
 {
-
-    [SerializeField] private GameObject enemy;
-    [SerializeField] private Transform parent;
+    public GameObject enemy;
     public float spawnTime = 5.0f;
-    public const string ON_SPAWN_ENEMY = "SPAWN_ENEMY";
-    [SerializeField] private List<GameObject> enemiesSpawned = new List<GameObject>();
-    public Transform goal;
+    public Transform[] spawnPoints;
+    
 
     // Use this for initialization
     void Start()
     {
         InvokeRepeating("SpawnEnemy", spawnTime, spawnTime);
-        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.destination = goal.position;
     }
 
     // Update is called once per frame
     void SpawnEnemy()
     {
-        GameObject spawn = GameObject.Instantiate(this.enemy, this.parent);
-        spawn.SetActive(true);
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
-        Vector3 position = this.enemy.transform.localPosition;
-        position.x = Random.Range(-4f, 4f);
-        position.z = Random.Range(-4f, 4f);
-
-        spawn.transform.localPosition = position;
-
-        spawn.transform.LookAt(Camera.main.transform);
-
-        enemiesSpawned.Add(spawn);
-
-        
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
 }
---
+++ Assets/Scripts/EnemyManager/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform goal;
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        goal = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(goal.position);
+    }
+}
