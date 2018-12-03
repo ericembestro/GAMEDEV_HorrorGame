0413781ca478078f86f22ba7a319b9c3d4d1aaeb
Lattrellino <Lattrellino@gmail.com>
2018-12-03 19:56:58 +0800

    AddedEnemyAttackect.
    enemyAttack, Player Health

--- Assets/Scripts/EnemyManager/EnemyMovement.cs
+++ Assets/Scripts/EnemyManager/EnemyMovement.cs
@@ -7,6 +7,8 @@ public class EnemyMovement : MonoBehaviour {
 
     Transform goal;
     NavMeshAgent agent;
+   
+    
 
     // Use this for initialization
     void Start () {
@@ -16,6 +18,21 @@ public class EnemyMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
         agent.SetDestination(goal.position);
+        
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Output the Collider's GameObject's name
+        Debug.Log(collision.collider.name);
+        if (collision.gameObject.CompareTag("Barricade"))
+        {
+            Destroy(collision.gameObject);
+        }
+
     }
 }
+
